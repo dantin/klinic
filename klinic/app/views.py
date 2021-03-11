86f5ad2a1fe5104b68f6d45517d3625854914a48
dantin <chengjie.ding@gmail.com>
2021-03-11 17:55:47 +0800

    add zh description.

--- klinic/app/views.py
+++ klinic/app/views.py
@@ -11,6 +11,10 @@ from .models import Device, Hospital, Department, DeviceBrand, DeviceType
 
 class DeviceModelView(ModelView):
     datamodel = SQLAInterface(Device)
+    list_title = _('List Device')
+    show_title = _('Show Device')
+    add_title = _('Add Device')
+    edit_title = _('Edit Device')
 
     list_columns = [
         'no',
@@ -20,12 +24,24 @@ class DeviceModelView(ModelView):
         'device_type',
     ]
 
+    label_columns = {
+        'no': _('Device No.'),
+        'name': _('Device Name'),
+        'department.hospital.name': _('Hospital Name'),
+        'department': _('Department'),
+        'device_type': _('Device Type'),
+        'apm_id': _('APM ID'),
+    }
+
     base_order = ('id', 'asc')
 
     show_fieldsets = [
-        ('Summary', {'fields': ['no', 'name']}),
         (
-            'Device Info',
+            _('Summary'),
+            {'fields': ['no', 'name']}
+        ),
+        (
+            _('Device Info'),
             {
                 'fields': [
                     'no',
@@ -41,6 +57,15 @@ class DeviceModelView(ModelView):
 class DepartmentModelView(ModelView):
     datamodel = SQLAInterface(Department)
     list_columns = ['hospital.name', 'name']
+    list_title = _('List Department')
+    show_title = _('Show Department')
+    add_title = _('Add Department')
+    edit_title = _('Edit Department')
+
+    label_columns = {
+        'hospital.name': _('Hospital Name'),
+        'name': _('Department Name'),
+    }
 
 
 class HospitalModelView(ModelView):
@@ -49,11 +74,26 @@ class HospitalModelView(ModelView):
         DepartmentModelView,
     ]
     list_title = _('List Hospital')
+    show_title = _('Show Hospital')
+    add_title = _('Add Hospital')
+    edit_title = _('Edit Hospital')
+
+    label_columns = {
+        'name': _('Hospital Name'),
+    }
 
 
 class DeviceTypeModelView(ModelView):
     datamodel = SQLAInterface(DeviceType)
     list_columns = ['device_brand.name', 'name']
+    list_title = _('List Device Type')
+    show_title = _('Show Device Type')
+    add_title = _('Add Device Type')
+    edit_title = _('Edit Device Type')
+    label_columns = {
+        'device_brand.name': _('Device Brand Name'),
+        'name': _('Device Type'),
+    }
 
 
 class DeviceBrandModelView(ModelView):
@@ -61,6 +101,13 @@ class DeviceBrandModelView(ModelView):
     related_views = [
         DeviceTypeModelView,
     ]
+    list_title = _('List Device Brand')
+    show_title = _('Show Device Brand')
+    add_title = _('Add Device Brand')
+    edit_title = _('Edit Device Brand')
+    label_columns = {
+        'name': _('Device Brand'),
+    }
 
 
 """
